192d1ebe8bda8e85139091ce2e5523e03b3a0615
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-05-28 18:40:44 +0200

    Calculate results with filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
 import { Grid } from "@mui/material";
 import "@fontsource/roboto/300.css";
@@ -7,34 +7,33 @@ import "@fontsource/roboto/400.css";
 import GlobalStyle from "./components/GlobalStyle";
 import GridElement from "./components/GridElement";
 
-import CuisineDropDown from "./components/CuisineDropDown";
-import DistrictDropDown from "./components/DistrictDropDown";
-import PlaceTypeDropDown from "./components/PlaceTypeDropDown";
 
-import GetResultButton from "./components/GetResultButton";
+import { PlaceTypeDropdown, DistrictDropdown, CuisineDropdown } from "./components/Dropdowns";
+
+import GetResultButton from "./components/ResultButton";
 import TitleHeader from "./components/TitleHeader";
 
 import ResultCalculation from "./components/ResultCalculation";
 
-import { PlaceTypeContext } from "./context";
+import { PlaceTypeContext, DistrictContext, CuisineContext } from "./context";
 
-import { Place } from "./types";
-
-// export const PlaceTypeContext = createContext({
-//   chosenPlaceType: "",
-//   setChosenPlaceType: (chosenPlaceType: string) => {} 
-// })
 
 const App: React.FC = () => {
 
   const [chosenPlaceType, setChosenPlaceType] = useState<string>("");
+  const [chosenDistrict, setChosenDistrict] = useState<string>("");
+  const [chosenCuisine, setChosenCuisine] = useState<string>("");
 
-  // console.log(chosenPlaceType)
+  console.log(chosenPlaceType)
+  console.log(chosenDistrict)
+  console.log(chosenCuisine)
 
   return (
     <>
 
     <PlaceTypeContext.Provider value={{ chosenPlaceType, setChosenPlaceType }}>
+    <DistrictContext.Provider value={{ chosenDistrict, setChosenDistrict }}>
+    <CuisineContext.Provider value={{ chosenCuisine, setChosenCuisine }}>
 
 
     <GlobalStyle />
@@ -48,31 +47,29 @@ const App: React.FC = () => {
           <TitleHeader />
         </div>
 
-
         <Box sx={{ flexGrow: 1 }}>
           <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
 
             <Grid item xs={2} sm={4} md={4}>
               <GridElement>
-                  <PlaceTypeDropDown/>
+                  <PlaceTypeDropdown/>
               </GridElement>
             </Grid>
 
             <Grid item xs={2} sm={4} md={4}>
               <GridElement>
-                  <DistrictDropDown />
+                  <DistrictDropdown />
               </GridElement>
             </Grid>
 
             <Grid item xs={2} sm={4} md={4}>
               <GridElement>
-                  <CuisineDropDown />
+                  <CuisineDropdown />
               </GridElement>
             </Grid>
 
           </Grid>
         </Box>
-
 
         <div className="get-result-button">
           <GetResultButton />
@@ -84,8 +81,10 @@ const App: React.FC = () => {
 
       </div>
 
+      </CuisineContext.Provider>
+      </DistrictContext.Provider>
       </PlaceTypeContext.Provider>
-  
+
     </>
   );
 };
--- src/components/ResultCalculation.tsx
+++ src/components/ResultCalculation.tsx
@@ -1,28 +1,70 @@
 import React, { useContext } from "react";
-// import { places } from "../data";
-// import { Cuisine, District, PlaceType } from "../types";
+import { places } from "../data";
+import { PlaceTypeContext, DistrictContext, CuisineContext } from "../context";
+import { Place } from "../types";
 
 const ResultCalculation: React.FC = () => {
 
-    // const { chosenPlaceType, setChosenPlaceType } = useContext(PlaceTypeContext)
+    const { chosenPlaceType } = useContext(PlaceTypeContext)
+    const { chosenDistrict } = useContext(DistrictContext)
+    const { chosenCuisine } = useContext(CuisineContext)
+
+    const baseResults = places
+    .filter((place) => {
+        return place.category.includes(chosenPlaceType)
+    })
+    .filter((place) => {
+        return place.district.includes(chosenDistrict)
+    })
+
+    // for now, an additional filter due to error with optional Place.cuisine property
+    const results = places
+    .filter((place) => {
+        return place.category.includes(chosenPlaceType)
+    })
+    .filter((place) => {
+        return place.district.includes(chosenDistrict)
+    }).filter((place) => {
+        return place.cuisine?.includes(chosenCuisine)
+    })
+
+    console.log(results)
+
+    if (baseResults.length === 0 && results.length === 0) {
+
+        return (
+            <div>
+                We don't currently have anything that matches your criteria. Try 
+                setting one of the boxes to "No Preference" to broaden your search!
+            </div>
+        )
     
-    return <span>hello</span>
+    } else {
 
-    // return <span>{chosenPlaceType}</span>
-
-    // const filteredByPlaceType = (value: PlaceType) => {
-    //     return places.filter(place => place.category.includes(value))
-    // };
-
-    // const filteredByDistrict = (value: District) => {
-    //     return places.filter(place => place.district.includes(value))
-    // };
-
-    // const filteredByCuisine = (value: Cuisine) => {
-    //     return places.filter(place => place.cuisine?.includes(value))
-    // };
+    return (
+        <div>
+            {!chosenCuisine ? (
+                <ul>
+                    {baseResults.map((result: Place) => (
+                        <li>
+                            {result.name}
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+            <ul>
+                {results.map((result: Place) => (
+                    <li>
+                        {result.name}
+                    </li>
+                ))}
+            </ul>
+            )}
+        </div>
+    )
 
 }
 
+};
 
 export default ResultCalculation;
